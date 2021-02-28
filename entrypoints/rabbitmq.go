50b69cf67927fd95626ebd5e01dd2302d42f37d4
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-02-28 02:36:36 +0100

    correctly sending emails in dev mode

--- entrypoints/rabbitmq.go
+++ entrypoints/rabbitmq.go
@@ -1,16 +1,50 @@
 package entrypoints
 
 import (
+	"fmt"
 	"log"
+	"net/smtp"
 
 	"github.com/streadway/amqp"
+
 	"guitou.com/notification-msvc/config"
+	"guitou.com/notification-msvc/models"
 )
 
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
 	}
+}
+
+// Request contains sending email parameter
+type Request struct {
+	from    string
+	to      []string
+	subject string
+	body    string
+}
+
+// NewRequest create
+func NewRequest(to []string, subject, body string) *Request {
+	return &Request{
+		to:      to,
+		subject: subject,
+		body:    body,
+	}
+}
+
+// SendEmail sends email
+func (r *Request) SendEmail(auth smtp.Auth) (bool, error) {
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	subject := "Subject: " + r.subject + "!\n"
+	msg := []byte(subject + mime + "\n" + r.body)
+	addr := "smtp.mailtrap.io:25"
+
+	if err := smtp.SendMail(addr, auth, "Guitou <notification@guitou.cm>", r.to, msg); err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 // InitBroker init the connection with AMQP
@@ -49,6 +83,26 @@ func InitBroker() {
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
+
+			data := models.NewProjectInvitationData()
+			err := data.ParseData(d.Body)
+			if err != nil {
+				failOnError(err, fmt.Sprintf("Unmarsheling Failed : %s", d.Body))
+			}
+			log.Println("DATA", data)
+
+			auth := smtp.PlainAuth("", "39150c7f22ec69", "55963de64c6833", "smtp.mailtrap.io")
+			body, err := data.ParseTemplate("templates/guitou.project.user.invited.html")
+			if err == nil {
+				subject := fmt.Sprintf("Join %s", data.ProjectName)
+				r := NewRequest([]string{data.UserEmail}, subject, body)
+
+				ok, _ := r.SendEmail(auth)
+				log.Println(ok)
+			} else {
+				log.Println("ERROR occured when sending the e-mail")
+				log.Println(err)
+			}
 		}
 	}()
 
--- models/models.go
+++ models/models.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"bytes"
+	"encoding/json"
+	"html/template"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -8,17 +12,50 @@ import (
 
 // Data is generic for all Data
 type Data struct {
-	ID   primitive.ObjectID `bson:"_id,omitempty"`
-	Type string             `bson:"type,omitempty"`
+	ID   primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Type string             `bson:"type,omitempty" json:"type,omitempty"`
 }
 
 // ProjectInvitationData contains data for user.invited.project event
 type ProjectInvitationData struct {
 	*Data
-	ProjectID    string `bson:"projectId,omitempty"`
-	ProjectName  string `bson:"projectName,omitempty"`
-	UserEmail    string `bson:"userEmail,omitempty"`
-	InvitationID string `bson:"invitationId,omitempty"`
+	ProjectID    string `json:"projectId,omitempty" bson:"projectId,omitempty"`
+	ProjectName  string `json:"projectName,omitempty" bson:"projectName,omitempty"`
+	UserEmail    string `json:"email,omitempty" bson:"userEmail,omitempty"`
+	InvitationID string `json:"invitationId,omitempty" bson:"invitationId,omitempty"`
+}
+
+// NewProjectInvitationData create a data
+func NewProjectInvitationData() *ProjectInvitationData {
+	return &ProjectInvitationData{}
+}
+
+// ParseData from JSON
+func (data *ProjectInvitationData) ParseData(body []byte) error {
+	err := json.Unmarshal(body, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ParseTemplate load the corresponding HTML Template
+func (data *ProjectInvitationData) ParseTemplate(templateFileName string) (string, error) {
+	log.Println("Project ID", data.ProjectID)
+
+	t, err := template.ParseFiles(templateFileName)
+	if err != nil {
+		return "", err
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return "", err
+	}
+
+	log.Println("Template loaded... ", buf.String())
+
+	return buf.String(), nil
 }
 
 // Email sent to the user
